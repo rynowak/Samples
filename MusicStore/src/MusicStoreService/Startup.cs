c90d9b724bbcdcbd146ba3959819ad0c92cf5830
Glenn Condron <glennc@microsoft.com>
2018-04-19 13:04:06 -0700

    Revert services to 2.0 to avoid MySql and EF bug. Change MusicStoreUI to be able to do a cf push. I would like to try and remove the need for scripts as much as possible.

--- MusicStore/src/MusicStoreService/Startup.cs
+++ MusicStore/src/MusicStoreService/Startup.cs
@@ -40,7 +40,6 @@ namespace MusicStore
 
             // Add Framework services
             services.AddMvc();
-
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
